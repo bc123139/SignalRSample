528764afbf3d19a2c7dc0f7a712b00fb2311dabc
Usman Tahir <Usman.tahir@synergy-it.com>
2022-06-26 23:35:45 +0500

    Notification client setup

---
+++ SignalRSample/wwwroot/js/notification.js
@@ -0,0 +1,29 @@
+﻿//create connection
+var connectionNotification = new signalR.HubConnectionBuilder()
+    .withUrl("/hubs/notification").build();
+
+
+document.getElementById("sendButton").disabled = true;
+
+document.getElementById("sendButton").addEventListener("click", function (event) {
+    var message = document.getElementById("notificationInput").value;
+    connectionNotification.send("SendMessage", message).then(function () {
+        document.getElementById("notificationInput").value = "";
+    });
+    event.preventDefault();
+});
+
+
+
+//start connection
+function fulfilled() {
+    //do something on start
+    console.log("Connection to User Hub Successful");
+    document.getElementById("sendButton").disabled = false;
+}
+
+function rejected() {
+    //rejected logs
+}
+
+connectionNotification.start().then(fulfilled, rejected);
